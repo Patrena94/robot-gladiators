03f474bf05843c53cb8cbd79a1d9e37a04444bae
Vashti <patrena@sbcglobal.net>
2021-06-10 09:55:57 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,10 +10,17 @@ var playerAttack =10;
 //You can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var fight = function (enemyName) {
+  fight(enemyRobot);
+}
+for(var i=0; i <enemyNames.length; i++) {console.log(enemyNames[i]);
+console.log(i); 
+console.log(enemyNames[i] + "is at" + i + "index");
+}
 var fight=function() {
     window.alert("Welcome to Robot Gladiators!");
 };
@@ -24,22 +31,23 @@ else if (promptFight==="skip" || promptFight ==="SKIP"){ var confirmSkip= window
 if (confirmSkip) {window.alert(playerName + "has decided to skip this fight. Goodbye!");
 playerMoney=playerMoney - 2;
 }
-else{
+else {
   fight();
 }
+
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
-console.log( playerName + "attacked" + enemyName +"." + enemyName + "now has" + enemyHealth + "health remaining."
+console.log( playerName + "attacked" + enemyNames +"." + enemyNames + "now has" + enemyHealth + "health remaining."
 );
-if(enemyHealth <=0) { window.alert(enemyName + "has died!");
+if(enemyHealth <=0) { window.alert(enemyNames + "has died!");
 }
-else {window.alert(enemyName +"still has"+ enemyHealth +"health left.");
+else {window.alert(enemyNames +"still has"+ enemyHealth +"health left.");
 }
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
-console.log( enemyName + " attacked " + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+console.log( enemyNames + " attacked " + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
   );
  if(playerHealth<=0) {window.alert(PalyerName +"has died");
 }
