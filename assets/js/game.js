3d62a85fd3784ea9d835c788d4f2d2f2806ae39c
Vashti <patrena@sbcglobal.net>
2021-06-15 10:52:58 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,29 @@
-function fight(){
-    window.alert("The fight has begun!");
-}
-//fight();
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth =100;
 var playerAttack =10;
 
-//You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+function fight(){
+    window.alert("The fight has begun!");
+}
+var fight = function(enemyName){...//fight function statements}
+
+for(var i=0; i<enemyNames.length; i++) {console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemynames[i] +"is at" + i +"index");
+}
+
+//You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 
 var fight = function (enemyName) {
 while(playerHealth > 0 && enemyHealth > 0){
@@ -34,14 +45,14 @@ enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
 console.log( playerName + "attacked" + enemyName +"." + enemyName + "now has" + enemyHealth + "health remaining."
 );
-if(enemyHealth <=0) { window.alert(enemyName + "has died!");break;
+if(enemyHealth <=0) { window.alert(enemyName + "has died!");
 }
 //award player money for winning playerMoney=playerMoney + 20;
 
 //leave while()loop since enemy is dead break; break;
-}
+} 
 else {
-  window.alert(enemyName +"still has" + enemyHealth + "health left.");
+  window.alert(enemyName + ' still has ' + playerHealth + ' health left.');
 }
 
 // remove players's health by subtracting the amount set in the enemyAttack variable
@@ -77,3 +88,8 @@ if(playerHealth===0) {console.log("this will not run.");
 }
 else{ console.log("This will run instead.");
 }
+//Games States
+//"WIN"-Player robto has defeated all enemy-robots.
+//*Fight all enemy-robots
+//*Defeat each enemy-robot
+//"LOSE"-Player robot's health is zero or less
