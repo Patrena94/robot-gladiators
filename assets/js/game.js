c4c527fe6fe48998a1edcf5ccb11be16625dab48
Vashti <patrena@sbcglobal.net>
2021-06-15 17:45:14 -0700

    updated shop and endgame

--- assets/js/game.js
+++ assets/js/game.js
@@ -88,6 +88,14 @@ for (var i = 0; i < enemyNames.length; i++) {
 
  // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
  fight(pickedEnemyName);
+ //if we're not at the last enemy in the array
+ if (playerHealth> 0 && i < enemyNames.length - 1){
+   // ask if player wants to use the store before next round
+   var storeConfirm = window.confirm ("the fight is over, visi the store before the next round?"); 
+   // if yes, take them to the store() function 
+   if(storeConfirm){shop();
+   }
+}
  }
   // if player isn't alive, stop the game
   else {
@@ -116,6 +124,42 @@ else {
 window.alert("Thank you for playing Robot Gladiators! Some back soon!");
 }
 }
-shop()
+var shop = function(){// ask player what they'd like to do
+  var shopOptionPrompt = window.prompt ("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice") ("entered the shop");
+// use switch to carry out action
+
+switch (shopOptionPrompt) {
+  case "refill"://new case
+  case "refill": 
+ 
+ if (playerMoney >= 7) {window.alert("Refilling player's health by 20 for 7 dollars.");
+//increase health and drease money
+playerHealth=playerHealth + 20;
+playerMoney=playerMoney -7;
+  }
+  else {window.alert("You don't have enough money!");
+}
+break;
+case "upgrade": //new case
+case "upgrade":
+if (playerMoney >= 7) {window.alert("Upgrading player's attack by 6 for 7 dollars.");
+//increase attack and decrease money
+playerAttack=playerAttack +6;
+playerMoney=playerMoney -7; 
+}
+else { window.alert("You Don't have enough money!")
+}
+break;
+case "leave": //new case
+case"LEAVE":
+  window.alert("Leaving the store");
+  //do nothing, so function will end
+  break;
+  default:
+    window.alert("You did not pick a valid option. Try again");
+    // call shop() again to force player to pick a valid option shop();
+    break;
+}
+};
 // start game when the page loads
 startGame();
